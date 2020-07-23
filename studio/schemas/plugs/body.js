6ea620d364b91f4d98c21d1d410c8bd45a0ee510
Luke Miller <lukem1293@gmail.com>
2020-07-23 10:22:25 -0500

    updated body section

--- studio/schemas/plugs/body.js
+++ studio/schemas/plugs/body.js
@@ -4,5 +4,17 @@ export default {
   type: 'object',
   fields: [
     { name: 'content', type: 'bodyPortableText' }
-  ]
+  ],
+  preview: {
+    select: {
+      subtitle: 'content.0.children.0.text'
+    },
+    prepare: ({ subtitle, title }) => {
+      const subText = subtitle.length > 70 ? `${subtitle.substring(0, 67)}...` : subtitle
+      return {
+        title: 'Body Section',
+        subtitle: subtitle ? subText : ''
+      }
+    }
+  }
 }
