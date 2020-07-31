efe11d60db9d3cc7eca9e28b15469b0416fbc57a
Luke Miller <lukem1293@gmail.com>
2020-07-31 15:39:44 -0500

    added thanks page\

---
+++ frontend/src/pages/thank-you.js
@@ -0,0 +1,45 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const menuArray = [
+  {
+    route: "/",
+    title: "Home",
+  },
+  {
+    route: "/about-me",
+    title: "About",
+  },
+  {
+    route: "/portfolio",
+    title: "Portfolio",
+  },
+  {
+    route: "/contact",
+    title: "Contact",
+  },
+]
+
+const ThankYou = () => {
+  return (
+    <Layout navMenuItems={menuArray}>
+      <SEO title="Thank You!" />
+      <section
+        className="wrapper"
+        style={{
+          minHeight: `61vh`,
+          display: `flex`,
+          flexDirection: `column`,
+          justifyContent: `center`,
+          alignItems: `center`,
+        }}
+      >
+        <h1>Thanks for reaching out! </h1>
+        <p>I'll be in touch shortly!</p>
+      </section>
+    </Layout>
+  )
+}
+
+export default ThankYou
